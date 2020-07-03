05dac0bdcfd3d0334222b5d36e032a5e61a368b0
pacna <paulc1231@gmail.com>
2020-07-03 19:07:24 -0400

    Update test

--- src/components/top-nav/top-nav.component.test.tsx
+++ src/components/top-nav/top-nav.component.test.tsx
@@ -4,6 +4,6 @@ import { TopNav } from './top-nav.component';
 
 test('renders REACT Game of Life', () => {
   const { getByText } = render(<TopNav />);
-  const linkElement = getByText(/REACT Game of Life/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/REACT Game of Life/i);
+  expect(text).toBeInTheDocument();
 });
